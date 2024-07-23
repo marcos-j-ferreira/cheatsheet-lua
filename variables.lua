bc14fd93fb33bdc5659c0eb32bf71956745ec7e3
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-22 21:50:32 -0300

    variables in lua

---
+++ variables.lua
@@ -0,0 +1,19 @@
+local a,b
+
+a = 10
+
+b = 15
+
+print("O valor de a:",a)
+
+print("O valor de b:", b)
+
+a,b = b, a 
+
+print("O valor de a:",a)
+
+print("O valor de b:", b)
+
+local f = 11 / 23
+
+print("O valor de f:",f)
