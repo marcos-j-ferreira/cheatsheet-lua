bccd512a5b3778f49fc10b8dcb2016c2be3033b8
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-22 21:25:46 -0300

    data type

---
+++ data_type.lua
@@ -0,0 +1,9 @@
+name = "marcos"
+age = 19
+dead = false
+
+print(type(name))   --[[Type String]]
+print(type(age))    --[[ number ]] 
+print(type(dead))   --[[true and false]]
+print(type(print))  --[[functio]]
+print(type(nil))    --[[nil]]
