403108e0b48050b1a7f20145134e3e0790cd0ed0
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-23 08:39:32 -0300

    function

---
+++ function.lua
@@ -0,0 +1,19 @@
+function printtax(x)
+    local tax = x * 0.21
+    print(tax)
+end
+printtax(76)
+
+
+function sum(a,b)
+
+    local plus =  a + b
+    return plus
+end
+
+a = 10 
+b = 10
+
+x = sum(a,b)
+
+print(x)
